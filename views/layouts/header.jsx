d85ced89d49b501aae1f1c24863af55e62631f5a
Adelie Fournier <Adelie@DST534.local>
2016-03-02 09:27:30 +0100

    responsive

--- views/layouts/header.jsx
+++ views/layouts/header.jsx
@@ -8,7 +8,7 @@ var HeaderLayout = React.createClass({
     render: function() {
 
         return (
-            <header>
+            <header className="box">
                 <FlexContainerLayout nameClass="headerFirstBar">
                     <h1><a href="#"><img src="medias/logo_pm.png" alt="Rakuten Group Priceminister" /></a></h1>
                     <ImgBtnLayout name="Panier" icon="cart "></ImgBtnLayout>
