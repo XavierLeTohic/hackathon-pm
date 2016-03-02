d1fe3c6291899f1fb88042e2e27e9e441fc97cfb
Adelie Fournier <Adelie@DST534.local>
2016-03-02 10:24:25 +0100

    chouchenn

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express'),
+		stylus = require('stylus'),
+		chouchenn = require('chouchenn'),
     routes = require('./routes'),
     jsxEngine = require('express-react-views'),
     app = module.exports.app = exports.app = express();
@@ -12,6 +14,18 @@ app.use(express.static('./public/build'));
 app.use(express.static('./public/libs'));
 
 routes(app);
+
+function compile(str, path) {
+  return stylus(str)
+  .set('filename', path)
+  .set('compress', true)
+  .use(chouchenn());
+}
+
+app.use(stylus.middleware({
+  src: __dirname
+  , compile: compile
+}));
 
 app.listen(1337, function() {
     console.log('Listening on port 1337...');
